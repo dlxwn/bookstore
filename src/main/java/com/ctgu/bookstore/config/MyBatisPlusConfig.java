2186a46f9f4c27195f93c9efc17e6dcf123ae71d
triwhiter <245886461@qq.com>
2020-06-09 12:32:48 +0800

    修改mybatis配置类注解

--- src/main/java/com/ctgu/bookstore/config/MyBatisPlusConfig.java
+++ src/main/java/com/ctgu/bookstore/config/MyBatisPlusConfig.java
@@ -2,6 +2,8 @@ package com.ctgu.bookstore.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @program: BookStore
@@ -9,6 +11,8 @@ import org.springframework.context.annotation.Bean;
  * @author: Linn
  * @create: 2020-06-06 10:37
  **/
+@EnableTransactionManagement(proxyTargetClass = true)
+@Configuration
 public class MyBatisPlusConfig {
     @Bean
     public PaginationInterceptor paginationInterceptor() {
